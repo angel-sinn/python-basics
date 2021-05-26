79f492e0e274772a133b30997f3224f2524752f0
Angel <angelkysinn@gmail.com>
2021-05-26 14:43:13 -0700

    Data types & numbers

---
+++ 02.numbers.py
@@ -0,0 +1,26 @@
+# Integer & Float
+print(2 + 5)
+print(2 - 5)
+print(2 * 5)
+print(2 / 5)
+
+print(type(2 + 5)) # int
+print(type(2 / 5)) # float
+print(type(0.00001)) # float
+
+# float takes up more memory than int
+
+print(type(20 + 1.1)) # converts to float
+
+# Power of
+print(2 ** 3) # 8
+
+# Double divide
+print(2 // 4) # 0
+
+# Modulo
+print(5 % 4) # 1
+
+# Math functions
+print(round(3.1))
+print(abs(-20)) # absolute value, pos numbers
