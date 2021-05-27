e9ef404e201bef1748062bfb0a08ac0398c8d2bf
Angel <angelkysinn@gmail.com>
2021-05-27 14:16:41 -0700

    Exercise 4, 5, 6

---
+++ exercises/exercise06.py
@@ -0,0 +1,10 @@
+# ----- Exercise 6 -----
+some_list = ['a','b','c','d','b','m','n','n']
+
+duplicates = []
+for value in some_list:
+  if some_list.count(value) > 1:
+    if value not in duplicates:
+      duplicates.append(value)
+
+print(duplicates)
