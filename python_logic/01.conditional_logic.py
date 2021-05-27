bcf647ded91d61cf0974998cd89e9057e862e42c
Angel <angelkysinn@gmail.com>
2021-05-26 21:56:01 -0700

    Conditional logic, truthy/falsy and ternary operator

---
+++ python_logic/01.conditional_logic.py
@@ -0,0 +1,9 @@
+is_old = True
+is_licensed = True
+
+if is_old and is_licensed:
+  print('You can drive :)')
+else:
+  print('You can\'t drive :(')
+
+print('Print this line')
---
+++ python_logic/03.ternary_operator.py
@@ -0,0 +1,4 @@
+is_friend = True
+can_message = "msesage allowed" if is_friend else "not allowed to message"
+
+print(can_message)
