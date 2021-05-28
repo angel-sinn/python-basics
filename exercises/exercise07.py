a50589fe2c71f9f8b6326573e5beccb3640fc7cd
Angel <angelkysinn@gmail.com>
2021-05-27 17:41:58 -0700

    Exercise 7 and 8

---
+++ exercises/exercise07.py
@@ -0,0 +1,22 @@
+# ----- Exercise 7 -----
+def checkDriverAge():
+    age = input("What is your age?: ")
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+
+checkDriverAge()
+
+def checkDriverAge2(age=0):
+    if int(age) < 18:
+        print("Sorry, you are too young to drive this car. Powering off")
+    elif int(age) > 18:
+        print("Powering On. Enjoy the ride!");
+    elif int(age) == 18:
+        print("Congratulations on your first year of driving. Enjoy the ride!")
+
+checkDriverAge2()
+checkDriverAge2(20)
